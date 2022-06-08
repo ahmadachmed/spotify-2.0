9c6bc16a18759981405072920c9700b818ef0a2d
Ahmad Ilham <ahmad.ilham@amartha.com>
2022-06-08 20:05:20 +0700

    first commit

---
+++ atoms/playlistAtom.js
@@ -0,0 +1,13 @@
+import { atom } from "recoil";
+
+export const playlistState = atom({
+  key: "playlistState",
+  default: null
+})
+
+export const playlistIdState = atom({
+  key: "playlistIdState",
+  default: "6lj5BskK1Lxmv4Ua1U4CtC",
+});
+
+
